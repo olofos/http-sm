14bb005dc9491d30b818570c60e37dfed282a18e
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2019-02-28 18:27:34 +0100

    Added http_base64_encode

--- src/http-util.c
+++ src/http-util.c
@@ -88,5 +88,69 @@ unsigned http_base64_encode(char *dest, const char *src, unsigned len)
 
 unsigned http_base64_decode(char *dest, const char *src, unsigned len)
 {
-    return 0;
+    static const char decode_tab[256] = {
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  62,   0,   0,   0,  63,
+        52,  53,  54,  55,  56,  57,  58,  59,  60,  61,   0,   0,   0,   0,   0,   0,
+         0,   0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
+        15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,   0,   0,   0,   0,   0,
+         0,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,
+        41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+         0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    };
+
+    if(!src || !dest || !len || len % 4) {
+        return 0;
+    }
+
+    const uint8_t *s = (const uint8_t *)src;
+
+    unsigned pad = (src[len-1] == '=') + (src[len-2] == '=');
+
+    unsigned n = 0;
+    unsigned i = 0;
+
+    while(i + 4 <= len - pad) {
+        uint8_t in1 = decode_tab[s[i++]];
+        uint8_t in2 = decode_tab[s[i++]];
+        uint8_t in3 = decode_tab[s[i++]];
+        uint8_t in4 = decode_tab[s[i++]];
+
+        uint8_t out1 = (in1 << 2) | (in2 >> 4);
+        uint8_t out2 = (in2 << 4) | (in3 >> 2);
+        uint8_t out3 = (in3 << 6) | in4;
+
+        dest[n++] = out1;
+        dest[n++] = out2;
+        dest[n++] = out3;
+    }
+
+    if(pad == 1) {
+        uint8_t in1 = decode_tab[s[i++]];
+        uint8_t in2 = decode_tab[s[i++]];
+        uint8_t in3 = decode_tab[s[i++]];
+
+        uint8_t out1 = (in1 << 2) | (in2 >> 4);
+        uint8_t out2 = (in2 << 4) | (in3 >> 2);
+
+        dest[n++] = out1;
+        dest[n++] = out2;
+    } else if(pad == 2) {
+        uint8_t in1 = decode_tab[s[i++]];
+        uint8_t in2 = decode_tab[s[i++]];
+
+        uint8_t out1 = (in1 << 2) | (in2 >> 4);
+
+        dest[n++] = out1;
+    }
+
+    return n;
 }
--- test/test_http-util.c
+++ test/test_http-util.c
@@ -205,6 +205,158 @@ static void test__http_base64_encode__returns_zero_if_dest_is_null(void **state)
     assert_int_equal(http_base64_encode(NULL, "012", 3) , 0);
 }
 
+static void test__http_base64_encode__does_not_write_outside_buffer(void **state)
+{
+
+    char dest[64] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0, 0x78, 0 };
+
+    http_base64_encode(dest+1, "a", 1);
+    assert_string_equal(dest, ((const char[]) { 0x12, 'Y', 'Q', '=', '=', 0xBC, 0xDE, 0xF0, 0x78, 0 }));
+
+    http_base64_encode(dest+1, "bc", 2);
+    assert_string_equal(dest, ((const char[]) { 0x12, 'Y', 'm', 'M', '=', 0xBC, 0xDE, 0xF0, 0x78, 0 }));
+
+    http_base64_encode(dest+1, "def", 3);
+    assert_string_equal(dest, ((const char[]) { 0x12, 'Z', 'G', 'V', 'm', 0xBC, 0xDE, 0xF0, 0x78, 0 }));
+
+    http_base64_encode(dest+1, "ghij", 4);
+    assert_string_equal(dest, ((const char[]) { 0x12, 'Z', '2', 'h', 'p', 'a', 'g', '=', '=', 0 }));
+}
+
+static void test__http_base64_encode__can_encode_all_values(void **state)
+{
+    char src[256];
+    char dest[350];
+    const char *expect[] = {
+        "AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4"
+        "OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3Bx"
+        "cnN0dXZ3eHl6e3x9fn+AgYKDhIWGh4iJiouMjY6PkJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmq"
+        "q6ytrq+wsbKztLW2t7i5uru8vb6/wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t/g4eLj"
+        "5OXm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+/w==",
+        "AQIDBAUGBwgJCgsMDQ4PEBESExQVFhcYGRobHB0eHyAhIiMkJSYnKCkqKywtLi8wMTIzNDU2Nzg5"
+        "Ojs8PT4/QEFCQ0RFRkdISUpLTE1OT1BRUlNUVVZXWFlaW1xdXl9gYWJjZGVmZ2hpamtsbW5vcHFy"
+        "c3R1dnd4eXp7fH1+f4CBgoOEhYaHiImKi4yNjo+QkZKTlJWWl5iZmpucnZ6foKGio6Slpqeoqaqr"
+        "rK2ur7CxsrO0tba3uLm6u7y9vr/AwcLDxMXGx8jJysvMzc7P0NHS09TV1tfY2drb3N3e3+Dh4uPk"
+        "5ebn6Onq6+zt7u/w8fLz9PX29/j5+vv8/f7/AA==",
+        "AgMEBQYHCAkKCwwNDg8QERITFBUWFxgZGhscHR4fICEiIyQlJicoKSorLC0uLzAxMjM0NTY3ODk6"
+        "Ozw9Pj9AQUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVpbXF1eX2BhYmNkZWZnaGlqa2xtbm9wcXJz"
+        "dHV2d3h5ent8fX5/gIGCg4SFhoeIiYqLjI2Oj5CRkpOUlZaXmJmam5ydnp+goaKjpKWmp6ipqqus"
+        "ra6vsLGys7S1tre4ubq7vL2+v8DBwsPExcbHyMnKy8zNzs/Q0dLT1NXW19jZ2tvc3d7f4OHi4+Tl"
+        "5ufo6err7O3u7/Dx8vP09fb3+Pn6+/z9/v8AAQ=="
+    };
+
+    for(int n = 0; n < 3; n++) {
+        for(int i = 0; i < 256; i++) {
+            src[i] = i+n;
+        }
+
+        unsigned ret = http_base64_encode(dest, src, 256);
+        dest[ret] = 0;
+        assert_string_equal(dest,expect[n]);
+    }
+}
+
+
+
+
+static void test__http_base64_decode__can_decode_strings_with_zero_byte_padding(void **state)
+{
+    const char *s;
+    char dest[64];
+    unsigned res;
+
+    s = "YWJj";
+    res = http_base64_decode(dest, s, strlen(s));
+    dest[res] = 0;
+    assert_string_equal(dest, "abc");
+
+    s = "YWJjZGVm";
+    res = http_base64_decode(dest, s, strlen(s));
+    dest[res] = 0;
+    assert_string_equal(dest, "abcdef");
+
+    s = "YWJjYWJjZGVm";
+    res = http_base64_decode(dest, s, strlen(s));
+    dest[res] = 0;
+    assert_string_equal(dest, "abcabcdef");
+}
+
+static void test__http_base64_decode__can_decode_strings_with_one_byte_padding(void **state)
+{
+    const char *s;
+    char dest[64];
+    unsigned res;
+
+    s = "YWI=";
+    res = http_base64_decode(dest, s, strlen(s));
+    dest[res] = 0;
+    assert_string_equal(dest, "ab");
+
+    s = "YWJjYWI=";
+    res = http_base64_decode(dest, s, strlen(s));
+    dest[res] = 0;
+    assert_string_equal(dest, "abcab");
+}
+
+static void test__http_base64_decode__can_decode_strings_with_two_bytes_padding(void **state)
+{
+    const char *s;
+    char dest[64];
+    unsigned res;
+
+    s = "YQ==";
+    res = http_base64_decode(dest, s, strlen(s));
+    dest[res] = 0;
+    assert_string_equal(dest, "a");
+
+    s = "YWJjYQ==";
+    res = http_base64_decode(dest, s, strlen(s));
+    dest[res] = 0;
+    assert_string_equal(dest, "abca");
+}
+
+static void test__http_base64_decode__returns_the_length_of_the_resulting_string(void **state)
+{
+    char dest[64];
+
+    assert_int_equal(http_base64_decode(dest, "YQ==", 4), 1);
+    assert_int_equal(http_base64_decode(dest, "YWI=", 4), 2);
+    assert_int_equal(http_base64_decode(dest, "YWJj", 4), 3);
+    assert_int_equal(http_base64_decode(dest, "YWJjYQ==", 8), 4);
+    assert_int_equal(http_base64_decode(dest, "YWJjYWI=", 8), 5);
+    assert_int_equal(http_base64_decode(dest, "YWJjYWJj", 8), 6);
+}
+
+static void test__http_base64_decode__returns_zero_if_dest_is_null(void **state)
+{
+    assert_int_equal(http_base64_decode(NULL, "YWJj", 4), 0);
+}
+
+static void test__http_base64_decode__returns_zero_if_src_is_null(void **state)
+{
+    char dest[64];
+    assert_int_equal(http_base64_decode(dest, NULL, 4), 0);
+}
+
+static void test__http_base64_decode__does_not_write_outside_buffer(void **state)
+{
+
+    char dest[64] = { 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0, 0 };
+
+    http_base64_decode(dest+1, "YQ==", 4);
+    assert_string_equal(dest, ((const char[]) { 0x12, 'a', 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0, 0 }));
+
+    http_base64_decode(dest+1, "YmM=", 4);
+    assert_string_equal(dest, ((const char[]) { 0x12, 'b', 'c', 0x78, 0x9A, 0xBC, 0xDE, 0xF0, 0 }));
+
+    http_base64_decode(dest+1, "ZGVm", 4);
+    assert_string_equal(dest, ((const char[]) { 0x12, 'd', 'e', 'f', 0x9A, 0xBC, 0xDE, 0xF0, 0 }));
+
+    http_base64_decode(dest+1, "Z2hpag==", 8);
+    assert_string_equal(dest, ((const char[]) { 0x12, 'g', 'h', 'i', 'j', 0xBC, 0xDE, 0xF0, 0 }));
+}
+
+
 const struct CMUnitTest tests_for_http_util[] = {
     cmocka_unit_test(test__http_hex_to_int__can_convert_hex_digits),
     cmocka_unit_test(test__http_hex_to_int__returns_zero_if_argument_is_not_a_digit),
@@ -226,6 +378,16 @@ const struct CMUnitTest tests_for_http_util[] = {
     cmocka_unit_test(test__http_base64_encode__returns_the_length_of_the_resulting_string),
     cmocka_unit_test(test__http_base64_encode__returns_zero_if_src_is_null),
     cmocka_unit_test(test__http_base64_encode__returns_zero_if_dest_is_null),
+    cmocka_unit_test(test__http_base64_encode__does_not_write_outside_buffer),
+    cmocka_unit_test(test__http_base64_encode__can_encode_all_values),
+
+    cmocka_unit_test(test__http_base64_decode__can_decode_strings_with_zero_byte_padding),
+    cmocka_unit_test(test__http_base64_decode__can_decode_strings_with_one_byte_padding),
+    cmocka_unit_test(test__http_base64_decode__can_decode_strings_with_two_bytes_padding),
+    cmocka_unit_test(test__http_base64_decode__returns_the_length_of_the_resulting_string),
+    cmocka_unit_test(test__http_base64_decode__returns_zero_if_src_is_null),
+    cmocka_unit_test(test__http_base64_decode__returns_zero_if_dest_is_null),
+    cmocka_unit_test(test__http_base64_decode__does_not_write_outside_buffer),
 };
 
 int main(void)
